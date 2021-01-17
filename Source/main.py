20f31460d144a083cf4c050e4529cd6a5c97c322
Angelina151612 <angelina151612@gmail.com>
2021-01-17 17:43:16 +0300

    fix main

--- Source/main.py
+++ Source/main.py
@@ -5,7 +5,8 @@ import getch
 from clean_user_data import cleaning
 from get_prediction import create_plots
 
-headline = """   
+
+headline = """
 ############################################################
 #                    Sentiment analysis                    #
 #                       with tweets                        #
@@ -13,7 +14,8 @@ headline = """
 
 
 def get_twits(name):
-    comand = f"twint -u {name} --since 2020-01-01 --until 2020-12-31 -o ../Users/{name}.csv --csv"
+    os.mkdir(f"../Users/{name}")
+    comand = f"twint -u {name} --since 2020-01-01 --until 2020-12-31 -o ../Users/{name}/{name}.csv --csv"
     process = subprocess.Popen(  # noqa
         comand.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
@@ -28,28 +30,24 @@ if not os.path.exists(r"../Users"):
     os.mkdir(r"../Users")
 while True:
     while True:
-        name = input("Enter username:") # noqa
+        name = input("Enter username:")  # noqa
 
         try:
-            path = f"../Users/{name}/"
-            """
-            if os.path.exists(f"{path}{name}.csv"):
-                print(f"\nPlots already exist! Please, check {path}")
+            path = f"../Users/{name}"
+            if os.path.exists(f"{path}/{name}.csv"):
+                print(f"\nPlots already exist! Please, check {path}")  # noqa
                 break
-                """
             get_twits(name)
         except ValueError:
             print("Wrong username! Please try again!")  # noqa
             continue
         else:
-            os.mkdir(f"../Users/{name}")
-            os.rename(f"../Users/{name}.csv", f"../Users/{name}/{name}.csv")
             cleaning(name)
             create_plots(name)
             print("\nAll plots was created!")  # noqa
             break
 
-    print("\nPress <Esc> to exit or any other key to continue.") # noqa
+    print("\nPress <Esc> to exit or any other key to continue.")  # noqa
     key = ord(getch.getch())
     if key == 27:  # ESC
         break
